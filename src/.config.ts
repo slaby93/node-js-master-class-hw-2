8ad3cc39984bacf8466a907a543e19112cbb323c
Daniel Słaby <pufek93@gmail.com>
2018-11-19 15:29:12 +0100

    Config

--- src/.config.ts
+++ src/.config.ts
@@ -1,12 +1,47 @@
-export default {
-  PORT: 3000,
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+interface IConfig {
+  PORT: number,
   MAILGUN: {
-    URL: 'api.mailgun.net',
-    DOMAIN: '',
-    AUTH: '',
+    URL: string,
+    DOMAIN: string,
+    AUTH: string,
   },
   STRIPE: {
-    URL: 'api.stripe.com',
-    SECRET_KEY: '',
+    URL: string,
+    SECRET_KEY: string,
   }
+}
+
+const config: { [index: string]: IConfig, } = {
+  production: {
+    PORT: 5000,
+    MAILGUN: {
+      URL: 'api.mailgun.net',
+      DOMAIN: '',
+      AUTH: '',
+    },
+    STRIPE: {
+      URL: 'api.stripe.com',
+      SECRET_KEY: '',
+    }
+  },
+  development: {
+    PORT: 3000,
+    MAILGUN: {
+      URL: 'api.mailgun.net',
+      DOMAIN: '',
+      AUTH: '',
+    },
+    STRIPE: {
+      URL: 'api.stripe.com',
+      SECRET_KEY: '',
+    }
+  }
+}
+
+export default {
+  ...(config[NODE_ENV] || config.development),
+  NODE_ENV
 }
